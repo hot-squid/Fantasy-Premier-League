2ddda9975e06d18e367367a8fb0caea0de23d7a4
hot-squid <t.burnand@ucl.ac.uk>
2024-10-09 22:22:30 +0100

    Updated file path

--- Code/player.py
+++ Code/player.py
@@ -53,7 +53,7 @@ columns = [
 data = data[columns]
 
 # Get extra player information
-player_info = pd.read_csv(r'C:\Users\thoma\Code\Projects\Fantasy-Premier-League\Data\Players\Accumulated\GW_6_24_25.csv')
+player_info = pd.read_csv(r'C:\Users\thoma\Code\Projects\Fantasy-Premier-League\Data\Players\Accumulated\GW_6.csv')
 
 # Extra player information to add
 columns = ['Player ID','Name', 'Last_Name', 'Team', 'Position']
