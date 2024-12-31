5f693b95de8e73ad72b8c1904f2b953ebf3eccc6
Thomas Burnand <151625241+hot-squid@users.noreply.github.com>
2024-12-31 13:37:09 +0000

    Update app.py

--- Website/app.py
+++ Website/app.py
@@ -8,7 +8,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Custom Themed App",
     layout="centered",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="closed",
     page_icon=None
 )
 
